41f07cf99d9a3523c92bc9f1a019e99e0f569b9a
Kandreas9 <andkarido@gmail.com>
2022-12-05 15:59:42 +0100

    register test page, user controller, and database

---
+++ app/public/controllers/Users.php
@@ -0,0 +1,144 @@
+<?php
+
+    require_once '../models/User.php';
+    require_once '../helpers/session_helper.php';
+
+    class Users {
+
+        private $userModel;
+        
+        public function __construct(){
+            $this->userModel = new User;
+        }
+
+        public function register(){
+            //Process form
+            
+            //Sanitize POST data
+            // $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            //Init data
+            $data = [
+                'username' => trim($_POST['username']),
+                'userEmail' => trim($_POST['userEmail']),
+                'userUid' => trim($_POST['userUid']),
+                'userPwd' => trim($_POST['userPwd']),
+                'pwdRepeat' => trim($_POST['pwdRepeat'])
+            ];
+
+            //Validate inputs
+            if(empty($data['username']) || empty($data['userEmail']) || empty($data['userUid']) || 
+            empty($data['userPwd']) || empty($data['pwdRepeat'])){
+                flash("register", "Please fill out all inputs");
+                redirect("../registerTest");
+            }
+
+            if(!preg_match("/^[a-zA-Z0-9]*$/", $data['userUid'])){
+                flash("register", "Invalid username");
+                redirect("`../registerTest`");
+            }
+
+            if(!filter_var($data['userEmail'], FILTER_VALIDATE_EMAIL)){
+                flash("register", "Invalid email");
+                redirect("../registerTest");
+            }
+
+            if(strlen($data['userPwd']) < 6){
+                flash("register", "Invalid password");
+                redirect("../registerTest");
+            } else if($data['userPwd'] !== $data['pwdRepeat']){
+                flash("register", "Passwords don't match");
+                redirect("../registerTest");
+            }
+
+            //User with the same email or password already exists
+            if($this->userModel->findUserByEmailOrUsername($data['userEmail'], $data['username'])){
+                flash("register", "Username or email already taken");
+                redirect("../registerTest");
+            }
+
+            //Passed all validation checks.
+            //Now going to hash password
+            $data['userPwd'] = password_hash($data['userPwd'], PASSWORD_DEFAULT);
+
+            //Register User
+            if($this->userModel->register($data)){
+                redirect("../login.php");
+            }else{
+                die("Something went wrong");
+            }
+        }
+
+    public function login(){
+        //Sanitize POST data
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+        //Init data
+        $data=[
+            'name/email' => trim($_POST['name/email']),
+            'usersPwd' => trim($_POST['usersPwd'])
+        ];
+
+        if(empty($data['name/email']) || empty($data['usersPwd'])){
+            flash("login", "Please fill out all inputs");
+            header("location: ../login.php");
+            exit();
+        }
+
+        //Check for user/email
+        if($this->userModel->findUserByEmailOrUsername($data['name/email'], $data['name/email'])){
+            //User Found
+            $loggedInUser = $this->userModel->login($data['name/email'], $data['usersPwd']);
+            if($loggedInUser){
+                //Create session
+                $this->createUserSession($loggedInUser);
+            }else{
+                flash("login", "Password Incorrect");
+                redirect("../login.php");
+            }
+        }else{
+            flash("login", "No user found");
+            redirect("../login.php");
+        }
+    }
+
+    public function createUserSession($user){
+        $_SESSION['usersId'] = $user->usersId;
+        $_SESSION['usersName'] = $user->usersName;
+        $_SESSION['usersEmail'] = $user->usersEmail;
+        redirect("../index.php");
+    }
+
+    public function logout(){
+        unset($_SESSION['usersId']);
+        unset($_SESSION['usersName']);
+        unset($_SESSION['usersEmail']);
+        session_destroy();
+        redirect("../index.php");
+    }
+}
+
+    $init = new Users;
+
+    //Ensure that user is sending a post request
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        switch($_POST['type']){
+            case 'register':
+                $init->register();
+                break;
+            case 'login':
+                $init->login();
+                break;
+            default:
+            redirect("../index.php");
+        }
+        
+    }else{
+        switch($_GET['q']){
+            case 'logout':
+                $init->logout();
+                break;
+            default:
+            redirect("../index.php");
+        }
+    }
---
+++ app/public/models/User.php
@@ -0,0 +1,73 @@
+<?php
+require_once '../libraries/Database.php';
+
+class User {
+
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database;
+    }
+
+    //Find user by email or username
+    public function findUserByEmailOrUsername($email, $username){
+        $this->db->query('SELECT * FROM users WHERE userUid = :username OR userEmail = :email');
+        $this->db->bind(':username', $username);
+        $this->db->bind(':email', $email);
+
+        $row = $this->db->single();
+
+        //Check row
+        if($this->db->rowCount() > 0){
+            return $row;
+        }else{
+            return false;
+        }
+    }
+
+    //Register User
+    public function register($data){
+        $this->db->query('INSERT INTO users (username, userEmail, userUid, userPwd) 
+        VALUES (:name, :email, :Uid, :password)');
+        //Bind values
+        $this->db->bind(':name', $data['username']);
+        $this->db->bind(':email', $data['userEmail']);
+        $this->db->bind(':Uid', $data['userUid']);
+        $this->db->bind(':password', $data['userPwd']);
+
+        //Execute
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Login user
+    public function login($nameOrEmail, $password){
+        $row = $this->findUserByEmailOrUsername($nameOrEmail, $nameOrEmail);
+
+        if($row == false) return false;
+
+        $hashedPassword = $row->userPwd;
+        if(password_verify($password, $hashedPassword)){
+            return $row;
+        }else{
+            return false;
+        }
+    }
+
+    //Reset Password
+    public function resetPassword($newPwdHash, $tokenEmail){
+        $this->db->query('UPDATE users SET userPwd=:pwd WHERE userEmail=:email');
+        $this->db->bind(':pwd', $newPwdHash);
+        $this->db->bind(':email', $tokenEmail);
+
+        //Execute
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}

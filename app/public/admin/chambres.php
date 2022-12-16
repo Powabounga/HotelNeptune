3b5e7bf645a5bb41fe829bcdbfc76551fc68930b
LilPowa <thomas.ramon@epsi.fr>
2022-12-16 11:46:46 +0100

    add chambre page editing for admin page

---
+++ app/public/admin/chambres.php
@@ -0,0 +1,38 @@
+<?php
+include_once '../models/User.php';
+
+$model = new User;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../global.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Chambres Id</th>
+            <th>Type Chambre</th>
+            <th>Prix</th>
+        </tr>
+        <?php foreach ($model->getAllChambres() as $key => $chambre) : ?>
+        <tr>
+            <td><?php echo ($chambre->chambresId); ?></td>
+            <td><?php echo ($chambre->type_chambre); ?></td>
+            <td><?php echo ($chambre->prix); ?></td>
+            <?php
+                echo ("<td><a href=\"/admin/users/edit?userId=$chambre->chambresId\">Edit</a></td>")
+                ?>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
--- app/public/models/User.php
+++ app/public/models/User.php
@@ -1,16 +1,19 @@
 <?php
 require_once '../libraries/Database.php';
 
-class User {
+class User
+{
 
     private $db;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = new Database;
     }
 
     //Find user by email or username
-    public function findUserByEmailOrUsername($email, $username){
+    public function findUserByEmailOrUsername($email, $username)
+    {
         $this->db->query('SELECT * FROM users WHERE userUid = :username OR userEmail = :email');
         $this->db->bind(':username', $username);
         $this->db->bind(':email', $email);
@@ -18,30 +21,40 @@ class User {
         $row = $this->db->single();
 
         //Check row
-        if($this->db->rowCount() > 0){
+        if ($this->db->rowCount() > 0) {
             return $row;
-        }else{
+        } else {
             return false;
         }
     }
 
-    public function getAllUsersCount() {
+    public function getAllUsersCount()
+    {
         $this->db->query('SELECT COUNT(userId) FROM users');
-        return $this->db->fetchColumn(); 
+        return $this->db->fetchColumn();
     }
 
-    public function getAllChambresCount() {
+    public function getAllChambresCount()
+    {
         $this->db->query('SELECT COUNT(chambresId) FROM chambres');
-        return $this->db->fetchColumn(); 
+        return $this->db->fetchColumn();
     }
 
-    public function getAllUsers() {
+    public function getAllUsers()
+    {
         $this->db->query('SELECT * FROM users');
         return $this->db->resultSet();
     }
 
+    public function getAllChambres()
+    {
+        $this->db->query('SELECT * FROM chambres');
+        return $this->db->resultSet();
+    }
+
     //Register User
-    public function register($data){
+    public function register($data)
+    {
         $this->db->query('INSERT INTO users (username, userEmail, userUid, userPwd) 
         VALUES (:name, :email, :Uid, :password)');
         //Bind values
@@ -51,37 +64,39 @@ class User {
         $this->db->bind(':password', $data['userPwd']);
 
         //Execute
-        if($this->db->execute()){
+        if ($this->db->execute()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
     //Login user
-    public function login($nameOrEmail, $password){
+    public function login($nameOrEmail, $password)
+    {
         $row = $this->findUserByEmailOrUsername($nameOrEmail, $nameOrEmail);
-        
-        if($row == false) return false;
+
+        if ($row == false) return false;
 
         $hashedPassword = $row->userPwd;
-        if(password_verify($password, $hashedPassword)){
+        if (password_verify($password, $hashedPassword)) {
             return $row;
-        }else{
+        } else {
             return false;
         }
     }
 
     //Reset Password
-    public function resetPassword($newPwdHash, $tokenEmail){
+    public function resetPassword($newPwdHash, $tokenEmail)
+    {
         $this->db->query('UPDATE users SET userPwd=:pwd WHERE userEmail=:email');
         $this->db->bind(':pwd', $newPwdHash);
         $this->db->bind(':email', $tokenEmail);
 
         //Execute
-        if($this->db->execute()){
+        if ($this->db->execute()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
